c52a7e679d67c0502631615b6db72fabc9fc1c13
Will Griffin <willgriffin@gmail.com>
2026-01-15 21:25:29 -0700

    feat: Upgrade Fabric server to Minecraft 1.21.11
    - Minecraft: 1.21.1 -> 1.21.11
    - Fabric API: 0.116.7 -> 0.141.1
    - FabricProxy-Lite: 2.10.1 -> 2.11.0
    
    Fixes version mismatch with client running 1.21.11.

--- images/fabric/mods.nix
+++ images/fabric/mods.nix
@@ -1,31 +1,31 @@
 { pkgs }:
 let
-  minecraftVersion = "1.21.1";
+  minecraftVersion = "1.21.11";
   fabricLoaderVersion = "0.18.4";
   installerVersion = "1.1.1";
 in {
   # Fabric Server Launcher
   fabricServer = pkgs.fetchurl {
     url = "https://meta.fabricmc.net/v2/versions/loader/${minecraftVersion}/${fabricLoaderVersion}/${installerVersion}/server/jar";
-    hash = "sha256-X6L7m3zEImLXUxiJ2gYEBDxZ5YbAEqJLXQxd382AvUE=";
+    hash = "sha256-YGkfY/kNWs4ojo8JZOKmnDyqnaFIp9G6ecjAC8zwJaM=";
   };
 
   # Fabric API - Required by most mods
   fabricApi = pkgs.fetchurl {
-    url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/m6zu1K31/fabric-api-0.116.7+1.21.1.jar";
-    hash = "sha256-CAGMxIyXQVo4AWoA29Wix6Rgumt6BRaQqMs9u16EgqQ=";
+    url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/DdVHbeR1/fabric-api-0.141.1+1.21.11.jar";
+    hash = "sha256-ald/g72LM8lAQSfRZTGsycQZX0feA5WVfJ1M0J17mMY=";
   };
 
   # FabricProxy-Lite - Velocity modern forwarding support
   fabricProxyLite = pkgs.fetchurl {
-    url = "https://cdn.modrinth.com/data/8dI2tmqs/versions/KqB3UA0q/FabricProxy-Lite-2.10.1.jar";
-    hash = "sha256-NnN7Ysel37Z5rD+san2xD5pCMxf7oZV0vE1HK0cRx0I=";
+    url = "https://cdn.modrinth.com/data/8dI2tmqs/versions/nR8AIdvx/FabricProxy-Lite-2.11.0.jar";
+    hash = "sha256-68er6vbAOsYZxwHrszLeaWbG2D7fq/AkNHIMj8PQPNw=";
   };
 
   # Version metadata for CI/CD
   meta = {
     inherit minecraftVersion fabricLoaderVersion installerVersion;
-    fabricApiVersion = "0.116.7+1.21.1";
-    fabricProxyLiteVersion = "2.10.1";
+    fabricApiVersion = "0.141.1+1.21.11";
+    fabricProxyLiteVersion = "2.11.0";
   };
 }
